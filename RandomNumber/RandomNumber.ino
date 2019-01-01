c79c6d83960b94cabcb09c6eaf47d4ea8c7aad8f
Valéry Farcy <vfarcy@free.fr>
2019-01-01 17:21:41 +0100

    Sur 16 bits !
    unsigned int est codé sur 16 bits, et non 32.

--- RandomNumber/RandomNumber.ino
+++ RandomNumber/RandomNumber.ino
@@ -6,13 +6,13 @@ void setup() {
 
 unsigned int RandomNumber() {
     unsigned int r = 0;
-    for (int i = 0; i < 32; ++i)
+    for (int i = 0; i < 16; ++i)
         r = (r << 1) | ((analogRead(A3)) & 1); //Reads the analog value on pin A3 
     return r;
 }
 
 void loop() {
-  Serial.print("A 32 bits random number coming from space, beyond and behind: ");
+  Serial.print("A 16 bits random number coming from space, beyond and behind: ");
   Serial.println(RandomNumber());
 }
 
